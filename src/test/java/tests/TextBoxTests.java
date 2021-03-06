5b79f210739b1daaa872cd8d54f9b60bc56d2881
Yuriy Logvinov <yuriy.logvinov@adonis.no>
2021-03-07 00:46:11 +0200

    07-mar-2021. small redesign

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -15,7 +15,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static org.openqa.selenium.Keys.CONTROL;
 import static org.openqa.selenium.Keys.ENTER;
 
 public class TextBoxTests {
@@ -61,9 +60,9 @@ public class TextBoxTests {
 
         int rand = new Random().nextInt(999999);
 
-        String fName="Fname" + rand;
-        String lName="Lname" + rand;
-        String email =format("e%s@mail.com", rand);
+        String fName = "Fname" + rand;
+        String lName = "Lname" + rand;
+        String email = format("e%s@mail.com", rand);
 
         $(By.id("firstName")).setValue(fName)
                 .pressEnter();
@@ -74,49 +73,45 @@ public class TextBoxTests {
 
         int gender = 1 + new Random().nextInt(3);
         $(By.id(format("gender-radio-%s", gender))).parent().click();
-        String genderName =  $(By.id(format("gender-radio-%s", gender))).getValue();
-        System.out.println("gender "+genderName);
+        String genderName = $(By.id(format("gender-radio-%s", gender))).getValue();
+        System.out.println("gender " + genderName);
 
         String mobileNum = "";
-        for (int i = 1; i <= 10; i++)
-            mobileNum = mobileNum + new Random().nextInt(10);
+        for (int i = 1; i <= 10; i++) mobileNum = mobileNum + new Random().nextInt(10);
         $(By.id("userNumber")).setValue(mobileNum);
 
-
-        String date = LocalDate.now().minusYears(22).minusDays(gender).format(ofPattern("dd MMM yyyy"));
+        LocalDate birthDate =  LocalDate.now().minusYears(22).minusDays(gender);
+        String date =birthDate.format(ofPattern("dd MMM yyyy"));
         $(By.id("dateOfBirthInput")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        $(By.id("dateOfBirthInput")).sendKeys(date+ENTER);
-
-        String dateFinal= $(By.id("dateOfBirthInput")).getText();
+        $(By.id("dateOfBirthInput")).sendKeys(date + ENTER);
 
         $(By.id("subjectsInput")).setValue("e")
                 .pressEnter();
 
         int hobby = 1 + new Random().nextInt(3);
-        $(By.id(format("hobbies-checkbox-%s", hobby))).parent().click();
+        $(By.id(format("hobbies-checkbox-%s", hobby))).parent()
+                .click();
 
         $(By.id("currentAddress")).setValue("country1\n city2\n street3 \n address4")
                 .pressEnter();
 
-        $(By.id("react-select-3-input")).setValue("utt").pressEnter();
-        $(By.id("react-select-4-input")).setValue("mer").pressEnter();
+        $(By.id("react-select-3-input")).setValue("utt")
+                .pressEnter();
+        $(By.id("react-select-4-input")).setValue("mer")
+                .pressEnter();
         $(By.id("submit")).click();
 
+        //
 
-        $(By.xpath(".//table//tr[1]/td[2]")).shouldHave(text(fName+" "+lName));
+        $(By.xpath(".//table//tr[1]/td[2]")).shouldHave(text(fName + " " + lName));
         $(By.xpath(".//table//tr[2]/td[2]")).shouldHave(text(email));
 
         $(By.xpath(".//table//tr[3]/td[2]")).shouldHave(text(genderName));
 
         $(By.xpath(".//table//tr[4]/td[2]")).shouldHave(text(mobileNum));
 
-        String date2 = LocalDate.now().minusYears(22).minusDays(gender).format(ofPattern("dd MMMM,yyyy"));
-        System.out.println("date2 "+date2);
-        System.out.println("datefinal "+dateFinal);
-
+        String date2 = birthDate.format(ofPattern("dd MMMM,yyyy"));
         $(By.xpath(".//table//tr[5]/td[2]")).shouldHave(text(date2));
-
-
     }
 
 }
