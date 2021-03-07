dfdf776ed4944d3dba3ac2965f5e75d9ed673ef1
Yuriy Logvinov <yuriy.logvinov@adonis.no>
2021-03-07 15:24:33 +0200

    07-mar-2021. xpath updated

--- src/test/java/tests/StudentRegistrationTest.java
+++ src/test/java/tests/StudentRegistrationTest.java
@@ -1,11 +1,8 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
+
 import helper.BaseTest;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
 import java.io.File;
@@ -17,11 +14,10 @@ import static com.codeborne.selenide.Selenide.*;
 import static java.lang.String.format;
 import static java.lang.System.getProperty;
 import static java.time.format.DateTimeFormatter.ofPattern;
-import static org.openqa.selenium.By.*;
+import static org.openqa.selenium.By.id;
 import static org.openqa.selenium.Keys.ENTER;
 
 public class StudentRegistrationTest extends BaseTest {
-
 
     @Test
     void testStudentRegistrationForm()  {
@@ -46,7 +42,6 @@ public class StudentRegistrationTest extends BaseTest {
         int gender = 1 + new Random().nextInt(3);
         $(id(format("gender-radio-%s", gender))).parent().click();
         String genderName = $(id(format("gender-radio-%s", gender))).getValue();
-        System.out.println("gender " + genderName);
 
         String mobileNum = "";
         for (int i = 1; i <= 10; i++) mobileNum = mobileNum + new Random().nextInt(10);
@@ -56,17 +51,13 @@ public class StudentRegistrationTest extends BaseTest {
         String date =birthDate.format(ofPattern("dd MMM yyyy"));
         $(id("dateOfBirthInput")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
         $(id("dateOfBirthInput")).sendKeys(date + ENTER);
-
         $(id("subjectsInput")).setValue("e")
                 .pressEnter();
-
         int hobby = 1 + new Random().nextInt(3);
         $(id(format("hobbies-checkbox-%s", hobby))).parent()
                 .click();
-
         File file = new File(getProperty("icon.path"));
         $(id("uploadPicture")).uploadFile(file);
-
         $(id("currentAddress")).setValue("country1\n city2\n street3 \n address4")
                 .pressEnter();
 
@@ -78,12 +69,10 @@ public class StudentRegistrationTest extends BaseTest {
 
         //Assert section
         $(id("example-modal-sizes-title-lg")).shouldHave(text("Thanks for submitting the form"));
-
         $x(".//table//tr[1]/td[2]").shouldHave(text(fName + " " + lName));
         $x(".//table//tr[2]/td[2]").shouldHave(text(email));
         $x(".//table//tr[3]/td[2]").shouldHave(text(genderName));
         $x(".//table//tr[4]/td[2]").shouldHave(text(mobileNum));
-
         String date2 = birthDate.format(ofPattern("dd MMMM,yyyy"));
         $x(".//table//tr[5]/td[2]").shouldHave(text(date2));
     }
